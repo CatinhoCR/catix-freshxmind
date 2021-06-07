52e2432cc019f393440d36e6a20fbbb23a9762d0
Cato <cato.castques@gmail.com>
2021-06-07 14:16:15 -0600

    Clean ups, adding the CPT and Tax files to the enqueued functions for reference.

--- sidebar.php
+++ sidebar.php
@@ -7,25 +7,12 @@
  *
  * @package FreshXMind
  */
-// $cat_arg = array(
-// 	'taxonomy' => 'category',
-// 	'class' => 'form-control',
-// 	// 'value_field' => 'term_id',
-// 	// 'selected' => $taxonomy_id,
-// 	'orderby' => 'name',
-// 	'show_count' => 0,
-// 	'hierarchical' => false,
-// 	'hide_if_empty' => true,
-// 	'walker'  => new FreshXMind_Custom_CategoryDropdown()
-// );
 // if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 // 	return;
 // }
+// if ( !is_active_sidebar('footer-1') || !is_active_sidebar('footer-2') ) {}
 ?>
 
 <aside id="secondary" class="widget-area">
-	<!-- <form action="<?php // echo esc_url(home_url('/')); ?>" method="get" class="fxm-form">
-		<?php // wp_dropdown_categories($cat_arg); ?>
-	</form> -->
 	<?php dynamic_sidebar(); ?>
 </aside><!-- #secondary -->
