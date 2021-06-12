9b70455b2a831e8ef7b13248b4c8b14f286e4b6e
Cato <cato.castques@gmail.com>
2021-06-12 04:37:25 -0600

    Release/version 0.9 (#11)
    * Updating spacing in readme
    
    * Feature/main theme blocks (#3)
    
    * Base work before switching back to simpler approach for now
    
    * Adding a few built in options to make this closer to a full ready to use starter theme with some basic initial settings and getters for quickier setup for things like cusotm colors, logos, etc
    
    * Feature/base theme functions (#4)
    
    * Some stuff to add to the starter
    
    * Working on footer and header
    
    * Clean ups, adding the CPT and Tax files to the enqueued functions for reference.
    
    * Updating basic functions, templates and readme
    
    * Basic Customizer API integration and Plugins support (#5)
    
    * Some stuff to add to the starter, will continue this later
    
    * Working on footer and header
    
    * Adding customizer support, might leave this for later tho
    
    * Added color settings basic need to extend
    
    * Adding more options to the theme customizer settings and some basic initial functions to make it output custom CSS and live preview with JS. JS file WIP. Additional settings will be added before feature is ready for review.
    
    * Setting up basic plugins support and function wrappers to prevent fatal errors
    
    * Adding basic WooCommerce class and workflow examples. (#9)
    
    Initial WC flow, needs to be extended. Will move on to creating a base sample theme from this template for easier identification of possible core built in features to this starter theme and circle back after that.

--- inc/compatibility/woocommerce/class-fxm-woocommerce.php
+++ inc/compatibility/woocommerce/class-fxm-woocommerce.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * WooCommerce Compatibility and Customizations
  *
@@ -9,20 +10,21 @@
  */
 
 if (!defined('ABSPATH') || !is_woocommerce_activated()) {
-  exit; // Exit if accessed directly.
+	exit; // Exit if accessed directly.
 }
 
 /**
  * Fresh X Mind WooCommerce Compatibility
  */
-if ( ! class_exists( 'Fxm_Woocommerce' ) ) :
+if (!class_exists('Fxm_Woocommerce')) :
 
 	/**
 	 * Fresh X Mind WooCommerce Compatibility
 	 *
 	 * @since 1.0.0
 	 */
-	class Fxm_Woocommerce {
+	class Fxm_Woocommerce
+	{
 
 		/**
 		 * Member Variable
@@ -34,8 +36,9 @@ if ( ! class_exists( 'Fxm_Woocommerce' ) ) :
 		/**
 		 * Initiator
 		 */
-		public static function get_instance() {
-			if ( ! isset( self::$instance ) ) {
+		public static function get_instance()
+		{
+			if (!isset(self::$instance)) {
 				self::$instance = new self();
 			}
 			return self::$instance;
@@ -44,10 +47,85 @@ if ( ! class_exists( 'Fxm_Woocommerce' ) ) :
 		/**
 		 * Constructor
 		 */
-		public function __construct() {}
-  }
+		public function __construct()
+		{
+
+			// WC Hooks to remove/override output
+			add_action('woocommerce_before_main_content', array($this, 'before_main_content_start'));
+			add_action('woocommerce_after_main_content', array($this, 'before_main_content_end'));
+			// add_action( 'wp', array( $this, 'woocommerce_init' ), 1 );
+		}
+
+		/**
+		 * Enqueue stylesheets
+		 *
+		 * @since 1.0.0
+		 */
+		public function add_styles()
+		{
+		}
+
+		/**
+		 * Register Customizer sections and panel for woocommerce
+		 *
+		 * @since 1.0.0
+		 * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+		 */
+		public function customize_register($wp_customize)
+		{
+		}
+
+		/**
+		 * Add start of wrapper
+		 */
+		public function before_main_content_start()
+		{
+			$site_sidebar = fxm_page_layout();
+			if ('left-sidebar' == $site_sidebar) {
+				get_sidebar();
+			}
+
+			// <div id="primary" class="content-area primary">
+
+			// fxm_content_top();
+
+			// <main id="main" class="site-main">
+			// <div class="fxm-woocommerce-container">
+
+		}
+
+		/**
+		 * Add end of wrapper
+		 */
+		public function before_main_content_end()
+		{
+			// </div> <!-- .fxm-woocommerce-container -->
+			// </main> <!-- #main -->
+			// fxm_content_after();
+			// </div> <!-- #primary -->
+			$site_sidebar = fxm_page_layout();
+			if ('right-sidebar' == $site_sidebar) {
+				get_sidebar();
+			}
+		}
+
+		/**
+		 * Remove WooCommerce Default Actions
+		 * To be replaced by custom content
+		 */
+		public function woocommerce_init()
+		{
+			remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+			remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+			remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
+			remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5);
+			remove_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10);
+			remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
+			remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10);
+		}
+	}
 endif;
 
-if ( apply_filters( 'fxm_enable_woocommerce_integration', true ) ) {
+if (apply_filters('fxm_enable_woocommerce_integration', true)) {
 	Fxm_Woocommerce::get_instance();
 }
